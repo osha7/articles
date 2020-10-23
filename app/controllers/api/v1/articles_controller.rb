f393fb9b6dddc0bbbc6da56330b922bf2a306222
osha7 <ogroetz@gmail.com>
2020-10-22 18:39:07 -0600

    full RESTful api

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -3,22 +3,38 @@ module Api
         class ArticlesController < ApplicationController
             def index
                 articles = Article.order('created_at DESC');
-                render json: {status: 'SUCCESS', message:'Loaded articles', data:articles},status: :ok
+                render json: {status: 'SUCCESS', message:'Loaded Articles', data:articles},status: :ok
             end
 
             def show
                 article = Article.find(params[:id])
-                render json: {status: 'SUCCESS', message:'Loaded article', data:article},status: :ok
+                render json: {status: 'SUCCESS', message:'Loaded Article', data:article},status: :ok
             end
 
             def create
                 article = Article.new(article_params)
 
                 if article.save
-                    render json: {status: 'SUCCESS', message:'Saved article', data:article},status: :ok
+                    render json: {status: 'SUCCESS', message:'Saved Article', data:article},status: :ok
                 else
                     render json: {status: 'ERROR', message:'Article Not Saved', data:article.errors},status: :unprocessable_entity
                 end
+            end
+
+            def update
+                article = Article.find(params[:id])
+
+                if article.update_attributes(article_params)
+                    render json: {status: 'SUCCESS', message:'Updated Article', data:article},status: :ok
+                else
+                    render json: {status: 'ERROR', message:'Article Not Saved', data:article.errors},status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: {status: 'SUCCESS', message:'Deleted Article', data:article},status: :ok
             end
 
             private
